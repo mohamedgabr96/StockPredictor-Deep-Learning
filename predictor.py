9cef0c0526c6cccd0408885e8203ee5ee1118970
Dawith Ha <dha8@illinois.edu>
2018-05-16 23:37:45 +0200

    Vanilla Version
    complete, modify/tune network and plot after

--- predictor.py
+++ predictor.py
@@ -1,5 +1,6 @@
 import requests
 import io
+import time
 import pandas as pd
 import numpy as np # keras takes numpy arrays, not dataframe :(
 from keras.models import Sequential
@@ -34,13 +35,18 @@ def create_dataset(data, feature_size = 1):
         a = data[i:(i+feature_size)]
         X.append(a)
         Y.append(data[i+feature_size])
-    return X,Y
+    return np.array(X),np.array(Y)
 
 
 # separate training (~2016) and test (2017~2018) data [0:4276]
 feature_size = 1
-X_train, Y_train = create_dataset(x_norm[0:4276])
-X_test, Y_test = create_dataset(x_norm[4277::])
+X_train, Y_train = create_dataset(x_norm[0:4276],feature_size)
+X_test, Y_test = create_dataset(x_norm[4277::],feature_size)
+
+# reshape data into 3D LSTM input [samples, timesteps, features]
+# see https://machinelearningmastery.com/reshape-input-data-long-short-term-memory-networks-keras/
+X_train = np.reshape(X_train, (X_train.shape[0], 1, feature_size))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, feature_size))
 
 # create LSTM network (modify/continue from here)
 model = Sequential()
@@ -56,7 +62,7 @@ model.add(LSTM(
     return_sequences=False))
 model.add(Dropout(0.2))
 
-model.add(Dense(output_dim=1))
+model.add(Dense(units=1))
 model.add(Activation('linear'))
 
 start = time.time()
@@ -64,13 +70,14 @@ model.compile(loss='mse', optimizer='rmsprop')
 print('compilation time : ', time.time() - start)
 
 # train model
-mode.fit(
+model.fit(
     X_train,
     Y_train,
     batch_size = 512,
-    nb_epoch=1,
+    epochs=1,
     validation_split=0.05)
 
 # predict and plot
-predictions = lstm.predict_sequences_multiple(model, X_test, 50, 50)
-lstm.plot_results_multiple(predictions, Y_test, 50)
+score = model.evaluate(X_test, Y_test, batch_size=512)
+#predictions = lstm.predict_sequences_multiple(model, X_test, 50, 50)
+#lstm.plot_results_multiple(predictions, Y_test, 50)
