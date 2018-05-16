280c48e6bcf6f8080906f5d0e6b4a5522e90b7a7
Dawith Ha <dha8@illinois.edu>
2018-05-16 18:45:05 +0200

    multiple features available through preprocessing
    each data could have multiple features(for subsequent vals after t), and the data after as y

--- predictor.py
+++ predictor.py
@@ -1,6 +1,9 @@
 import requests
 import io
 import pandas as pd
+import numpy as np # keras takes numpy arrays, not dataframe :(
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, LSTM
 
 # fetch csv file using Alpha Vantage api
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=INX&outputsize=full&datatype=csv&apikey=9B9U2G2YHKS9ME8T'
@@ -18,22 +21,20 @@ df.to_csv('data.csv')
 
 # preprocess data; only need daily_adjusted
 x_raw = df.loc[::-1,'adjusted_close'].values
-x_raw = pd.Series(x_raw)
 
 # normalize data
 x_norm = x_raw/x_raw[0] - 1
 
-# convert raw series data into x and y dataset. y looks is 1 day ahead by default
+# convert raw series data into x and y dataset. y = x(t+1)
 # representation with 1 input feature by default when using a stateful LSTM
 # alternatively, we can use multiple days. see https://machinelearningmastery.com/use-features-lstm-networks-time-series-forecasting/
-# ctrl+F for "Experiments with Features"
-def create_dataset(data, days_ahead = 1):
-    data = data.values
+def create_dataset(data, feature_size = 1):
     X, Y = [],[]
-    for i in range(data.size - days_ahead):
-        X.append(data[i])
-        Y.append(data[i+days_ahead])
-    return pd.Series(X),pd.Series(Y)
+    for i in range(data.size - feature_size - 1):
+        a = data[i:(i+feature_size)]
+        X.append(a)
+        Y.append(data[i+feature_size])
+    return X,Y
 
 
 # separate training (~2016) and test (2017~2018) data [0:4276]
@@ -43,23 +44,23 @@ X_test, Y_test = create_dataset(x_norm[4277::])
 # create LSTM network (modify/continue from here)
 model = Sequential()
 model.add(LSTM(
-    input_dim = 1,
+    input_shape = (1,),
     output_dim = 50,
     return_sequences=True))
+
 model.add(Dropout(0.2))
 
-mode.add(LSTM(
-    100,
-    return_sequence=False))
+model.add(LSTM(
+    100, # output space
+    return_sequences=False))
 model.add(Dropout(0.2))
 
-model.add(Dense(
-    output_dim=1))
+model.add(Dense(output_dim=1))
 model.add(Activation('linear'))
 
 start = time.time()
 model.compile(loss='mse', optimizer='rmsprop')
-print 'compilation time : ', time.time() - start
+print('compilation time : ', time.time() - start)
 
 # train model
 mode.fit(
