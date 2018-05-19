4c9f08aa43ecb0b8bdc3b59cb8069862855d2506
Mohamed Gabr <mohamed.gabr@hotmail.com>
2018-05-19 14:37:28 +0200

    Hyper-parameter optimization using randomization

--- predictor.py
+++ predictor.py
@@ -8,123 +8,157 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, LSTM
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+import random
 
 
-# fetch csv file using Alpha Vantage api
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=INX&outputsize=full&datatype=csv&apikey=9B9U2G2YHKS9ME8T'
-
-print('collecting data...')
-data = requests.get(url)
-df = pd.read_csv(io.StringIO(data.text),index_col=None)
-# df = pd.read_csv('data.csv')
-
-# checking to see the data was collected
-print(df.head())
-
-# save csv
-df.to_csv('data.csv')
-
-# pre_process data; only need daily_adjusted
-data_raw = df.loc[::-1,'adjusted_close'].values
-
-# normalize data
-data_norm = data_raw/data_raw[0] - 1
-
-# convert raw series data into x and y dataset. y = x(t+1)
-# representation with 1 input feature by default when using a stateful LSTM
-# alternatively, we can use multiple days. see https://machinelearningmastery.com/use-features-lstm-networks-time-series-forecasting/
-def create_dataset(data, feature_size = 1):
-    X, Y = [],[]
-    for i in range(data.size - feature_size - 1):
-        a = data[i:(i+feature_size)]
-        X.append(a)
-        Y.append(data[i+feature_size])
-    return np.array(X),np.array(Y)
 
 
-# separate training and test data
-feature_size = 240 ######
-train_size = round(data_norm.size* .6)
-X_train, Y_train = create_dataset(data_norm[0:train_size], feature_size)
-X_test, Y_test = create_dataset(data_norm[train_size::], feature_size)
-
-# reshape data into 3D LSTM input [samples, timesteps, features]
-# see https://machinelearningmastery.com/reshape-input-data-long-short-term-memory-networks-keras/
-X_train = np.reshape(X_train, (X_train.shape[0], 1, feature_size))
-X_test = np.reshape(X_test, (X_test.shape[0], 1, feature_size))
+epoch_range = [20,40]
+decay_range = [.9, .95]
+lr_range = [0.0000001, 0.0001]
+momentum_range = [0.8, 0.95]
+feature_size_range = [0, 400]
 
 
-# create LSTM network (modify/continue from here)
-model = Sequential()
-model.add(LSTM(
-    input_shape = (1,feature_size),
-    units = 25,  # output space #########
-    return_sequences=False)) ###### change to true if additional LSTM layers
+def try_random(trials):
+    scores_regroup = pd.DataFrame(data=None)
+    for i in range(0, trials):
+        epoch = int(random.uniform(epoch_range[0], epoch_range[1]))
+        print("Number of Epochs: " + str(epoch) + "\n")
+        decay = random.uniform(decay_range[0], decay_range[1])
+        lr = random.uniform(lr_range[0], lr_range[1])
+        momentum = random.uniform(momentum_range[0], momentum_range[1])
+        feature_size = int(random.uniform(feature_size_range[0], feature_size_range[1]))
+        print("Feature Size: " + str(feature_size) + "\n")
+        score = LSTM_1(epoch, decay, lr, momentum, feature_size)
+        print("The Score is: " + str(score) + "\n")
+        scores_regroup.append([epoch, decay, lr, momentum, feature_size, score])
+    scores_regroup.to_csv("Results.csv")
+    print(scores_regroup)
 
-#### possibly additional layers below ######
 
-model.add(Dropout(0.1)) ########## do we need?
+def LSTM_1(epoch, decay, lr, momentum, feature_size):
+    # fetch csv file using Alpha Vantage api
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=INX&outputsize=full&datatype=csv&apikey=9B9U2G2YHKS9ME8T'
 
-model.add(Dense(units=1)) # Xavier initialization, default no activation
+    print('collecting data...')
+    data = requests.get(url)
+    df = pd.read_csv(io.StringIO(data.text),index_col=None)
+    # df = pd.read_csv('data.csv')
 
-start = time.time()
+    # checking to see the data was collected
+    print(df.head())
 
-##### try different optimizers and loss fxn ###### below
-sgd = kr.optimizers.SGD(lr=0.0002, decay=1e-6, momentum=0.9, nesterov=True)
-rms = kr.optimizers.rmsprop(lr=0.00005, rho=0.9, epsilon=None, decay=0.0)
+    # save csv
+    df.to_csv('data.csv')
 
-model.compile(loss='mse', optimizer='adam')
-print('compilation time : ', time.time() - start)
+    # pre_process data; only need daily_adjusted
+    data_raw = df.loc[::-1,'adjusted_close'].values
 
-score = 1 - model.evaluate(X_test, Y_test, batch_size=240)
-print("The score before is " + str(score))
+    # normalize data
+    data_norm = data_raw/data_raw[0] - 1
 
-# train model ####try different batchsize, epoch, justify why validation split=0.4
-history = model.fit(
-    X_train,
-    Y_train,
-    batch_size=240,
-    epochs=100,
-    validation_split=0.4,
-    verbose=0)
+    # convert raw series data into x and y dataset. y = x(t+1)
+    # representation with 1 input feature by default when using a stateful LSTM
+    # alternatively, we can use multiple days. see https://machinelearningmastery.com/use-features-lstm-networks-time-series-forecasting/
+    def create_dataset(data, feature_size = 1):
+        X, Y = [],[]
+        for i in range(data.size - feature_size - 1):
+            a = data[i:(i+feature_size)]
+            X.append(a)
+            Y.append(data[i+feature_size])
+        return np.array(X),np.array(Y)
 
-# predict and plot
-score = 1 - model.evaluate(X_test, Y_test, batch_size=240)
-print(history.history)
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper right')
-plt.show()
-print("The score is " + str(score))
+    # separate training and test data
+   # feature_size = feature_size ######
+    train_size = round(data_norm.size* .6)
+    X_train, Y_train = create_dataset(data_norm[0:train_size], feature_size)
+    X_test, Y_test = create_dataset(data_norm[train_size::], feature_size)
 
-# get predictions
-train_predictions = model.predict(X_train)
-test_predictions = model.predict(X_test)
+    # reshape data into 3D LSTM input [samples, timesteps, features]
+    # see https://machinelearningmastery.com/reshape-input-data-long-short-term-memory-networks-keras/
+    X_train = np.reshape(X_train, (X_train.shape[0], 1, feature_size))
+    X_test = np.reshape(X_test, (X_test.shape[0], 1, feature_size))
 
-# plot normalized predictions
-plt.plot(test_predictions)
-plt.plot(Y_test)
-plt.title('Test Predictions vs Actual, Normalized')
-plt.ylabel('score')
-plt.xlabel('sequence(t)')
-plt.legend(['predictions','actual'],loc='upper right')
-plt.show()
 
-# de-normalize the predictions
-train_predictions = (train_predictions+1) * data_raw[0]
-test_predictions = (test_predictions+1) * data_raw[0]
-train_actual = (Y_train+1) * data_raw[0]
-test_actual = (Y_test+1) * data_raw[0]
+    # create LSTM network (modify/continue from here)
+    model = Sequential()
+    model.add(LSTM(
+        input_shape=(1, feature_size),
+        units=25,  # output space #########
+        return_sequences=False))  ###### change to true if additional LSTM layers
 
-# plot the denormalized predictions
-plt.plot(test_predictions)
-plt.plot(test_actual)
-plt.title('Test Predictions vs Actual, Denormalized')
-plt.ylabel('score')
-plt.xlabel('sequence(t)')
-plt.legend(['predictions','actual'],loc='upper right')
-plt.show()
+    #### possibly additional layers below ######
+
+    model.add(Dropout(0.1)) ########## do we need?
+
+    model.add(Dense(units=1))  # Xavier initialization, default no activation
+
+    start = time.time()
+
+    ##### try different optimizers and loss fxn ###### below
+    sgd = kr.optimizers.SGD(lr=lr, decay=decay, momentum=momentum, nesterov=True)
+    rms = kr.optimizers.rmsprop(lr=0.00005, rho=0.9, epsilon=None, decay=0.0)
+
+    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    print('compilation time : ', time.time() - start)
+
+
+
+    # train model ####try different batchsize, epoch, justify why validation split=0.4
+    history = model.fit(
+        X_train,
+        Y_train,
+        batch_size=240,
+        epochs=epoch,
+        validation_split=0.4,
+        verbose=0)
+
+    # predict and plot
+
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['val_loss'])
+    # plt.title('model loss')
+    # plt.ylabel('loss')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'test', 'accuracy'], loc='upper right')
+    # plt.show()
+    # print("The score is " + str(score))
+
+    scores, acc = model.evaluate(X_test, Y_test, batch_size=240)
+
+
+    return acc
+
+    # # get predictions
+    # train_predictions = model.predict(X_train)
+    # test_predictions = model.predict(X_test)
+    #
+    # # plot normalized predictions
+    # plt.plot(test_predictions)
+    # plt.plot(Y_test)
+    # plt.title('Test Predictions vs Actual, Normalized')
+    # plt.ylabel('score')
+    # plt.xlabel('sequence(t)')
+    # plt.legend(['predictions','actual'],loc='upper right')
+    # plt.show()
+    #
+    # # de-normalize the predictions
+    # train_predictions = (train_predictions+1) * data_raw[0]
+    # test_predictions = (test_predictions+1) * data_raw[0]
+    # train_actual = (Y_train+1) * data_raw[0]
+    # test_actual = (Y_test+1) * data_raw[0]
+    #
+    # # plot the denormalized predictions
+    # plt.plot(test_predictions)
+    # plt.plot(test_actual)
+    # plt.title('Test Predictions vs Actual, Denormalized')
+    # plt.ylabel('score')
+    # plt.xlabel('sequence(t)')
+    # plt.legend(['predictions','actual'],loc='upper right')
+    # plt.show()
+
+
+try_random(5)
+
