f9010c47a06d96b29508af9c4c562bc9e7d42583
Dave GCP <dawithha@gmail.com>
2018-05-23 13:18:26 +0000

    ready for MLP fine search

--- MLP_predictor.py
+++ MLP_predictor.py
@@ -15,11 +15,12 @@ from keras import backend as K
 K.tensorflow_backend._get_available_gpus()
 
 # set range for hyperparameters
-epoch_range = [1,400]
+epoch_range = [1,400] #700
 decay_range = [.9, .99]
 lr_range = [0.0000001, 0.001]
 momentum_range = [0.8, 0.95]
-feature_size_range = [1, 500]
+batch_size_range = [155,240]
+feature_size_range = [135,321]
 neuron_range = [5, 100]
 
 
@@ -55,7 +56,7 @@ def create_dataset(data, feature_size = 1):
     return np.array(X),np.array(Y)
 
 
-def train(X_train,Y_train,X_test,Y_test,lr,decay,momentum,feature_size,epoch):
+def train(X_train,Y_train,X_test,Y_test,lr,decay,momentum,feature_size,epoch,batch_size=240):
     # create Multilayered Perceptron network
     model = Sequential()
     model.add(Dense(25,input_dim=feature_size, activation='relu'))
@@ -78,15 +79,15 @@ def train(X_train,Y_train,X_test,Y_test,lr,decay,momentum,feature_size,epoch):
     history = model.fit(
         X_train,
         Y_train,
-        batch_size=240,
+        batch_size=batch_size,
         epochs=epoch,
         validation_split=0.4)
 
-    score = model.evaluate(X_test, Y_test, batch_size=240)
+    score = model.evaluate(X_test, Y_test, batch_size=batch_size)
     return model,history, score
 
 
-def Multi_Perceptron(lr, decay, momentum, feature_size, epoch):
+def Multi_Perceptron(lr, decay, momentum, feature_size, epoch, batch_size):
     # scrap the data
     raw_data = scrapdata()
     # difference the data to remove increasing trends
@@ -96,7 +97,7 @@ def Multi_Perceptron(lr, decay, momentum, feature_size, epoch):
     X_test, Y_test = create_dataset(raw_data[train_size::], feature_size)
     X_train = np.reshape(X_train, (X_train.shape[0],  feature_size))
     X_test = np.reshape(X_test, (X_test.shape[0],  feature_size))
-    return train(X_train, Y_train,X_test,Y_test, lr, decay, momentum, feature_size, epoch)
+    return train(X_train, Y_train,X_test,Y_test, lr, decay, momentum, feature_size, epoch, batch_size)
 
 
 def no_epochs_find(trials):
@@ -106,6 +107,7 @@ def no_epochs_find(trials):
     momentum_n = [.83,.89]
     lr_n = [.0002,.001]
     decay_n = [.91,.99]
+    batch_size=240
     fig, ax = plt.subplots()
     ax.set_color_cycle(['red', 'black'])
     for i in range(0, trials):
@@ -114,7 +116,7 @@ def no_epochs_find(trials):
         momentum = random.uniform(momentum_n[0], momentum_n[1])
         feature_size = int(random.uniform(feature_size_n[0], feature_size_n[1]))
 
-        model, history, score = Multi_Perceptron(lr, decay, momentum, feature_size, epochs)
+        model, history, score = Multi_Perceptron(lr, decay, momentum, feature_size, epochs, batch_size)
         histories.append(history)
         plt.plot(history.history['loss'])
         plt.plot(history.history['val_loss'])
@@ -128,28 +130,66 @@ def no_features_find(trials):
     histories = []
     epochs = 700
     feature_size_n = [30,390]
-    momentum_n = [.83,.89]
-    lr_n = [.0002,.001]
-    decay_n = [.91,.99]
+    momentum_n = .083572
+    lr = .000971
+    decay = .988072
+    batch_size=240
     fig, ax = plt.subplots()
     ax.set_color_cycle(['red', 'black'])
     for i in range(0, trials):
         print('trial '+str(i)+'\n')
-        lr = random.uniform(lr_n[0], lr_n[1])
-        decay = random.uniform(decay_n[0], decay_n[1])
-        momentum = random.uniform(momentum_n[0], momentum_n[1])
         feature_size = int(random.uniform(feature_size_n[0], feature_size_n[1]))
-
-        model, history, score = Multi_Perceptron(lr, decay, momentum, feature_size, epochs)
+        #hard set to best trial results, constant values
+        model, history, score = Multi_Perceptron(lr, decay, momentum, feature_size, epochs, batch_size)
         histories.append([feature_size, score])
     df = pd.DataFrame(data=histories, columns=['Feature Size', 'Score'])
     df.to_csv("MLP_Feature_Size_Loss.csv")
 
 
-'''
-results = try_random(200)
-results_df = pd.DataFrame(data= results, columns=['Epoch','Decay','Learning Rate','Momentum','Feature Size','Score'])
-results_df.to_csv("MLP_sgd_results.csv")
-'''
+def no_batch_find(trials):
+    histories = []
+    batch_size = int(random.uniform(1, 500))
+    epochs = 700
+    feature_size = 390
+    momentum = .083572
+    lr = .000971
+    decay = .988072
+    fig, ax = plt.subplots()
+    ax.set_color_cycle(['red', 'black'])
+    for i in range(0, trials):
+        print("ITERATION NUMBER " + str(i))
+        model, history, score = Multi_Perceptron(lr, decay, momentum, feature_size, epochs, batch_size)
+        histories.append([batch_size, score])
+        batch_size = int(random.uniform(1, 380))
+    df = pd.DataFrame(data=histories, columns=['batch size', 'Score'])
+    df.to_csv("MLP_Batch_Size_Loss.csv")
 
-no_features_find(50)
+
+def final_find(trials):
+    histories = []
+    epochs = 700 
+    feature_size_n = [135,321]
+    momentum_n = [.83,.89]
+    lr_n = [.0002,.001]
+    decay_n = [.91,.99]
+    batch_size_n=[155,240]
+    fig, ax = plt.subplots()
+    ax.set_color_cycle(['red', 'black'])
+    for i in range(0, trials):
+        print("ITERATION NUMBER " + str(i))
+        lr = random.uniform(lr_n[0], lr_n[1])
+        decay = random.uniform(decay_n[0], decay_n[1])
+        momentum = random.uniform(momentum_n[0], momentum_n[1])
+        feature_size = int(random.uniform(feature_size_n[0], feature_size_n[1]))
+        batch_size = int(random.uniform(batch_size_n[0],batch_size_n[1]))
+
+        model, history, score = Multi_Perceptron(lr, decay, momentum, feature_size, epochs, batch_size)
+        histories.append([lr,decay,momentum,feature_size,epochs,batch_size,score])
+    plt.savefig('MLP_loss.png')
+    df = pd.DataFrame(data=histories, columns=['lr', 'decay','momentum','feature_size','epochs','batch_size','score'])
+    df.to_csv("MLP_fine_search.csv")
+    
+
+
+
+no_batch_find(200)
